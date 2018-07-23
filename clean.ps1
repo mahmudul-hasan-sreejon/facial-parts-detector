90b8e8f5e9a7a2ccab737965aa78e7b5aea1a87a
Mahmudul Hasan <sreejonK19@users.noreply.github.com>
2018-07-23 17:02:38 +0600

    un-dockerized

--- clean.ps1
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# docker stop container_name
-# docker rm container_name
-# docker rmi -f image_name
-
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
--- run.ps1
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run --name test_app_container -it -d -p 8080:8080 app:latest
