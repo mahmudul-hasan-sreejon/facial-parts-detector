40e17a5550d505179299f836e3cb147453c80a72
Mahmudul Hasan <sreejonK19@users.noreply.github.com>
2018-07-26 01:14:08 +0600

    live camera feature updated

--- run.ps1
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-python app.py --shape-predictor shape_predictor_68_face_landmarks.dat --image images/example_01.jpg
-
-# python app.py --shape-predictor shape_predictor_68_face_landmarks.dat --image images/example_02.jpg
-
-# python app.py --shape-predictor shape_predictor_68_face_landmarks.dat --image images/example_03.jpg
